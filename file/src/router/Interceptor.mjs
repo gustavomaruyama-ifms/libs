5bbd55d732a7f8bfbb6219e729f08223424b3759
Charles Viegas <cviegas@azi.net.br>
2018-11-14 22:48:33 -0300

    REMOÇÃO DO enviroment para process.env

--- file/src/router/Interceptor.mjs
+++ file/src/router/Interceptor.mjs
@@ -1,17 +1,16 @@
 import multer from 'multer'
 import GridFsStorage from 'multer-gridfs-storage'
 import FilenameCreator from '../business/entity/FilenameCreator.mjs'
-import environment from '../common/environment.mjs'
 
 export default class Interceptor {
     static create() {
         const storage = GridFsStorage({
-            url: environment.db.url,
+            url: process.env.DB_URL,
             file: (req, file) => {
                 const filename = FilenameCreator.createFilename(file.originalname)
                 return {filename: filename, metadata: {originalname: file.originalname}}
             }
         })
-        return multer({storage: storage, limits: {fileSize: environment.file.maxSize}}).single('file')
+        return multer({storage: storage, limits: {fileSize: process.env.FILE_MAX_SIZE}}).single('file')
     }
 }
