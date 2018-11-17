e9cab71c5e072c53006debad5adac288b4e845e3
Charles Viegas <cviegas@azi.net.br>
2018-11-17 11:29:49 -0300

    atualização da senha

--- auth/src/business/usecase/UpdatePassword.mjs
+++ auth/src/business/usecase/UpdatePassword.mjs
@@ -4,13 +4,14 @@ import UserEntity from '../entity/UserEntity.mjs'
 import Token from '../entity/Token.mjs'
 
 export default class UpdatePassword {
-    async execute(userData, responder) {
+    async execute(credentials, responder) {
         try {
+            const userData = Token.verify(credentials.token)
             const matchedUser = await userRepository.findByEmail(userData.email)
             if (!matchedUser) {
                 throw new Error('EMAIL_NOT_EXISTS')
             }
-            matchedUser.password = await Encryption.hash(userData.password)
+            matchedUser.password = await Encryption.hash(credentials.password)
             const updatedUser = await userRepository.save(matchedUser)
             const payload = UserEntity.createPayload(updatedUser)
             const token = Token.create(payload)
--- auth/src/router/forgetPassword.mjs
+++ auth/src/router/forgetPassword.mjs
@@ -3,7 +3,7 @@ import Responder from '../common/Responder.mjs'
 import ForgetPassword from '../business/usecase/ForgetPassword.mjs'
 
 const router = express.Router()
-router.put('/auth/forgetpassword/:email', (req, res, next) => {
+router.post('/auth/password/:email', (req, res, next) => {
     const responder = new Responder(req, res, next)
     const forgetPassword = new ForgetPassword()
     forgetPassword.execute(req.params.email, responder)
